9618a545e68cb4307ccc8f12b0ca5eaf87726bf0
Teddy Economou <teddyecon@gmail.com>
2023-02-24 16:52:58 -0500

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return items[0] + ' and ' + items[1]
+    last_word = items.pop()
+    return ', '.join(items) + ", and " + last_word 
